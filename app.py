b7dd645fd9457f8ef1bbce8b0c4528919257d303
ri-gh <98031978+ri-gh@users.noreply.github.com>
2022-07-07 20:45:27 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,246 +1,290 @@
-from turtle import delay
-import pandas as pd
+import uvicorn
+import pandas as pd 
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-import plotly.express as px
-import plotly.graph_objects as go
-import plotly.io as pio
-import pip
-import streamlit as st
+from pydantic import BaseModel
+from typing import Literal, Union
+from fastapi import FastAPI
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import  OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
 
-pip.main(["install", "openpyxl"])
-
-st.set_page_config(
-    page_title="Delay Analysis Get Around",
-    page_icon="💸 ",
-    layout="wide"
-)
-
-st.title("⏰💸 Delay Analysis for 'Get Around' App 🚗")
-
-
-st.markdown("""
-    Welcome to this dashboard, we are going to analyze  delays and their consequences for next user,
-    what could be the solutions to minimize issues for next customer:
-    * threshold between 2 rentals: how long should the minimum delay be?
-    * scope of our feature: should we enable the feature for all cars? only Connect cars?
-
-    You can find our `rental prices' predictions API`  🤖 here :  https://fastapilast.herokuapp.com/.
-""")
-
-@st.cache (allow_output_mutation=True)
-def load_data(nrows):
-    data = pd.read_excel('get_around_delay_analysis.xlsx',nrows=nrows)
-    return data
-
-data = load_data(18000)
-
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
-
-st.markdown("---")
-
-st.subheader('Check in type Analysis')
-check_in_type = data['checkin_type'].unique()
-check_in = st.selectbox("Which type of check in would you like to see?", check_in_type)
-
-###number of delay for mobile:
-booking_type = len(data[data['checkin_type'] == check_in])/len(data)*100
-delay = len(data[(data['checkin_type'] == check_in) & (data['delay_at_checkout_in_minutes'] > 0)])/len(data)*100
-cancellation = len(data[(data['checkin_type'] == check_in) & (data['state'] == 'canceled')])/len(data)*100
-ended = len(data[(data['checkin_type'] == check_in) & (data['state'] == 'ended')])/len(data)*100
-
-
-content, empty_space = st.columns([3, 2])
-with empty_space:
-    st.empty()
-with content:
-    st.metric("Percentage of '{}' rentals".format(check_in),"{} %".format(np.round(booking_type,2)))
-    st.metric("Percentage of delay for '{}' rentals".format(check_in),"{} %".format(np.round(delay,2)))
-    #st.caption("Majority of delays are made by mobile type check in")
-
-st.markdown("  ")
-
-fig=px.bar(data,x=data['checkin_type'].value_counts(),y=data['checkin_type'].unique(),orientation='h',
-color=data['checkin_type'].unique(),width=800, height=400)
-fig.update_layout(
-        title="Global view of rentals check in type",
-    xaxis_title="Count",
-    yaxis_title="Rental type",
-    font=dict(family="Arial, monospace",
-        size=18,
-        ))
-
-st.write(fig)
-
-st.markdown("---")
-
-st.subheader("Global view of rentals state:")
-st.markdown("* We have {}% of cancellation".format(np.round((len(data[data['state'] == 'canceled'])/len(data['state'])*100)),2))
-st.markdown("* We have {}% of ended rental".format(np.round((len(data[data['state'] == 'ended'])/len(data['state'])*100)),2))
-
-st.markdown("  ")
-
-st.markdown("  ")
-
-df1 = data['state'].value_counts()
-st.bar_chart(df1)
-
-st.markdown("  ")
-
-check_in_type1 = data['checkin_type'].unique()
-check_in1 = st.selectbox("Which type of check in would you like to review?", check_in_type1)
-content, empty_space = st.columns([3, 2])
-
-cancelpertype = len(data[(data['checkin_type'] == check_in1) & (data['state'] == 'canceled')]) /len(data[data['checkin_type'] == check_in1])*100
-endedpertype = len(data[(data['checkin_type'] == check_in1) & (data['state'] == 'ended')]) /len(data[data['checkin_type'] == check_in1])*100
-
-with empty_space:
-    st.empty()
-with content:
-    st.metric("Percentage of cancelled rentals for '{}' rental type".format(check_in1),"{} %".format(np.round(cancelpertype,2)))
-    st.metric("Percentage of ended rental for '{}' rental type".format(check_in1),"{} %".format(np.round(endedpertype,2)))
-   
-st.markdown("  ")
-
-pie_chart_data = data.groupby('state')['checkin_type'].value_counts()
-fig1, ax1 = plt.subplots(figsize=(3,3))
-ax1.pie(pie_chart_data.values, labels=pie_chart_data.index,
-        textprops={'color':'white'},
-        autopct='%1.1f%%',
-       shadow=True, 
-       startangle=-15,
-        explode = (0.2,0.2,0,0),
-       radius=1.5)
-fig1.set_facecolor('black')
-plt.legend(bbox_to_anchor=(1.1, 1.05),loc = 'lower left', fontsize = 6)
-plt.title('Global state of rentals by kind of booking', x= 0, y= 1.3, color ='white', fontsize=9)
-plt.show()
-
-st.pyplot(fig1) 
-
-
-st.subheader('Delay Analysis ⏰')
-
-st.subheader("Global view of delays")
-
-@st.cache
-def load_data5(nrows):
-    data['late/on time'] = data['delay_at_checkout_in_minutes'].apply(lambda x :'late' if x > 0 else 'on time')
-    mask = data['delay_at_checkout_in_minutes'].notnull()
-    df = data.loc[mask].reset_index(drop = True)
-    return df
-
-df = load_data5(18000)
-
-w = df['late/on time'].value_counts()
-
-st.markdown("* The global percentage of delayed check out is {}% ".format(np.round(len(data[data['delay_at_checkout_in_minutes'] > 0])/len(data[data['delay_at_checkout_in_minutes'].notnull()])*100),2))
-
-@st.cache
-def load_datal(nrows):
-    tf = data[data['delay_at_checkout_in_minutes'] > 0].reset_index(drop = True)
-    tf['delay_at_checkout_in_minutes'] = tf['delay_at_checkout_in_minutes'][np.abs(tf['delay_at_checkout_in_minutes']-tf['delay_at_checkout_in_minutes'].mean())<=(3*tf['delay_at_checkout_in_minutes'].std())]
-    return tf
-tf = load_data5(18000)
-
-st.bar_chart(w)
-a= (np.round((tf['delay_at_checkout_in_minutes'].mean()),2))
-pd.to_datetime(64.58 , unit="%H%M").strftime('%H:%M')
-st.markdown(' * The average time delay is {} (in hour) '.format((pd.to_datetime(a , unit="%H%M").strftime('%H:%M'))))
-
-@st.cache
-def load_datax(nrows):
-    dfx = data[data['delay_at_checkout_in_minutes'] > 0]
-    return dfx
-dfx = load_datax(18000)
-
-st.markdown("* Check in type of delayed checked out people :")
-
-pie_chart_data1 = dfx['checkin_type'].value_counts()
-fig2, ax2 = plt.subplots(figsize=(2.5,2.5))
-ax2.pie(pie_chart_data1.values, labels=pie_chart_data1.index,
-        textprops={'color':'white'},
-        autopct='%1.1f%%',
-        startangle=40,
-        explode = (0.2,0.2),
-       radius=1.5)
-fig2.set_facecolor('black')
-plt.legend(bbox_to_anchor=(1.1, 1.05),loc = 'lower left', fontsize = 6)
-plt.show()
-
-st.pyplot(fig2)
-
-
-@st.cache
-def load_data1(nrows):
-    data_retard = (df[df['late/on time'] == 'late'].reset_index(drop = True))
-    return data_retard
-
-data_retard = load_data1(18000)
-
-if st.checkbox('Show raw list of late drivers'):
-    st.subheader('Raw delays data')
-    st.write(data_retard)
-
-st.markdown("---")
-
-st.subheader('Analysis of delay s consequences')
-
-
-retardataires_list = []
-for i in range(len(data_retard)):
-    retardataires_list.append(data_retard['rental_id'][i])
-
-
-
-@st.cache
-def load_data2(nrows):
-    dfa = (data[data['previous_ended_rental_id'].isin(retardataires_list)].reset_index(drop= True))
-    return dfa
-
-dfa=load_data2(18000)
-
-if st.checkbox('Show raw data of driver after getting the car with delay'):
-    st.subheader('Raw data of drivers getting their car with delay')
-    st.write(dfa)
-
-
-st.markdown("* There are {} delayed check out after getting his rental with delay ".format(len(dfa[dfa['late/on time'] == 'late'])))
-dfa.dropna()
-b = dfa['late/on time'].value_counts()
-st.bar_chart(b)
-
-
-
-
-st.markdown("* There are {} cancellations after a delayed check out ".format(len(dfa[dfa['state'] == 'canceled'])))
-
-c = dfa['state'].value_counts()
-st.bar_chart(c)
-
-
-st.subheader("Delay threshold & feature's scope 🤓:")
-data = data[(data['delay_at_checkout_in_minutes'] >= 120) & (data['checkin_type'] == 'mobile')]
-st.caption("- We choose to take a threshold of ***2 hours*** & to apply it on the ***'mobile'*** car rental scope")
-st.caption("- We will now be able to handle ***{}***  problematic cases (late check out /mobile check in)".format(len(data)))
+description = """
+Get Around API helps you to estimate the rental price per day for your car 🤑. 
  
 
-### Side bar 
-st.sidebar.header("Dashboard summary")
-st.sidebar.markdown("""
-    * [Check in type Analysis](#check-in-type-analysis)
-    * [Global view of rentals state](#global-view-of-rentals-state)
-    * [Delay Analysis ⏰](#delay-analysis)
-    * [Analysis of delay s consequences](#analysis-of-delay-s-consequences)
-    * [Delay threshold & feature's scope 🤓](#delay-threshold-feature-s-scope)
-    """)
-e = st.sidebar.empty()
-e.write("")
+## Preview
+
+Where you can: 
+* get a `/preview` of a few rows of your dataset
+* get `/unique-values` of a given column in your dataset
+* get `/max` value of a given column iny your dataset
+* get `/min` value of a given column iny your dataset
+* get `/price_per_car_model` mean of price per day for each car model_key
+
+## Machine-Learning
+
+* `/predict` you can estimate the rental price per day for your car
 
 
+Check out documentation for more information on each endpoint. 
+"""
+
+tags_metadata = [
+    {
+        "name": "Preview",
+        "description": "Endpoints that deal with general datas",
+    },
+    {
+        "name": "Machine-Learning",
+        "description": "Endpoint that uses our Machine Learning model to estimate rental price"
+    }
+
+]
+
+#on fait une instance 'app' de FastApi
+app = FastAPI(
+    title="🚗 Get Around API",
+    description=description,
+    version="0.1",
+    contact={
+        "name": "Get Around API - by R.G."
+    },
+    openapi_tags=tags_metadata
+)
+
+class PredictionFeatures(BaseModel):
+    model_key: Literal['Citroën', 'Peugeot', 'PGO', 'Renault', 'Audi', 'BMW', 'Ford',
+       'Mercedes', 'Opel', 'Porsche', 'Volkswagen', 'KIA Motors',
+       'Alfa Romeo', 'Ferrari', 'Fiat', 'Lamborghini', 'Maserati',
+       'Lexus', 'Honda', 'Mazda', 'Mini', 'Mitsubishi', 'Nissan', 'SEAT',
+       'Subaru', 'Suzuki', 'Toyota', 'Yamaha'] 
+    mileage: Union[int , float]
+    engine_power: Union[int, float ]
+    fuel: Literal['diesel', 'petrol', 'hybrid_petrol', 'electro']
+    paint_color: Literal['black', 'grey', 'white', 'red', 'silver', 'blue', 'orange',
+       'beige', 'brown', 'green']
+    car_type: Literal['convertible', 'coupe', 'estate', 'hatchback', 'sedan',
+       'subcompact', 'suv', 'van']
+    private_parking_available: Literal[True, False]
+    has_gps: Literal[True, False]
+    has_air_conditioning: Literal[True, False]
+    automatic_car: Literal[True, False]
+    has_getaround_connect: Literal[True, False]
+    has_speed_regulator : Literal[True, False]
+    winter_tires : Literal[True, False]
+    
+
+#on définit nos Endpoints
+# les decorateurs (@app.get par ex.)sont des fonctions qui appellent d'autres fonctions
+
+@app.get("/", tags=["Preview"])
+async def index():
+    """
+    Just saying Hello to you 
+    """
+    message = "Hello World 😁, welcome to the 'Get Around' predictions price API!"
+    return message
 
 
+@app.get("/preview", tags=["Preview"])
+async def random_rental(rows: int=10):
+    """
+    Get a sample of your whole dataset. 
+    You can specify how many rows you want by specifying a value for `rows`, default is `10`
 
+    """
+    df = pd.read_csv("get_around_pricing_project.csv")
+    sample = df.sample(rows)
+    return sample.to_json()
+
+
+@app.get("/unique-values", tags=["Preview"])
+async def unique_values(column: str = "fuel"):
+    """
+    Get unique values from a given column
+
+    You have to choose between one of those column names:
+
+    ['model_key', 'mileage', 'engine_power', 'fuel', 'paint_color',
+        'car_type', 'private_parking_available', 'has_gps',
+        'has_air_conditioning', 'automatic_car', 'has_getaround_connect',
+        'has_speed_regulator', 'winter_tires', 'rental_price_per_day']
+    """
+    
+    df = pd.read_csv("get_around_pricing_project.csv")
+    df = pd.Series(df[column].unique())
+    return df.to_json()
+          
+@app.get("/max", tags=["Preview"])
+async def max_values(column: str = "rental_price_per_day"):
+    """
+    Get max values from a given column 
+
+    You have to choose between one of those column names:
+
+    ['model_key', 'mileage', 'engine_power', 'fuel', 'paint_color',
+       'car_type', 'private_parking_available', 'has_gps',
+       'has_air_conditioning', 'automatic_car', 'has_getaround_connect',
+       'has_speed_regulator', 'winter_tires', 'rental_price_per_day']
+    """
+    
+    df = pd.read_csv("get_around_pricing_project.csv")
+    df = pd.Series(df[column].max())
+    return df.to_json()   
+
+
+@app.get("/min", tags=["Preview"])
+async def min_values(column: str = "rental_price_per_day"):
+    """
+    Get min values from a given column 
+
+    You have to choose between one of those column names:
+
+    ['model_key', 'mileage', 'engine_power', 'fuel', 'paint_color',
+       'car_type', 'private_parking_available', 'has_gps',
+       'has_air_conditioning', 'automatic_car', 'has_getaround_connect',
+       'has_speed_regulator', 'winter_tires', 'rental_price_per_day']
+    """
+    
+    df = pd.read_csv("get_around_pricing_project.csv")
+    df = pd.Series(df[column].min())
+    return df.to_json()
+
+@app.get("/price_per_car_model", tags=["Preview"])
+async def price_per_car_mean(car_brand: str = "Citroën"):
+    """
+    Get mean rental price per day per car model key
+
+    For model_key price you have to choose one of this value:
+
+    model_key: Literal['Citroën', 'Peugeot', 'PGO', 'Renault', 'Audi', 'BMW', 'Ford',
+       'Mercedes', 'Opel', 'Porsche', 'Volkswagen', 'KIA Motors',
+       'Alfa Romeo', 'Ferrari', 'Fiat', 'Lamborghini', 'Maserati',
+       'Lexus', 'Honda', 'Mazda', 'Mini', 'Mitsubishi', 'Nissan', 'SEAT',
+       'Subaru', 'Suzuki', 'Toyota', 'Yamaha'] 
+    """
+    
+    df = pd.read_csv("get_around_pricing_project.csv")
+    data_f = df[df['model_key']  == car_brand]
+    data_f = pd.Series(np.round((data_f['rental_price_per_day'].mean()),2))
+    
+    return data_f.to_json()
+
+@app.post("/predict",tags=["Machine-Learning"])
+async def predict(predictionFeatures: PredictionFeatures):
+    """
+    For each feature to predict price you have to choose one of this value or of same type:
+
+    model_key: Literal['Citroën', 'Peugeot', 'PGO', 'Renault', 'Audi', 'BMW', 'Ford',
+       'Mercedes', 'Opel', 'Porsche', 'Volkswagen', 'KIA Motors',
+       'Alfa Romeo', 'Ferrari', 'Fiat', 'Lamborghini', 'Maserati',
+       'Lexus', 'Honda', 'Mazda', 'Mini', 'Mitsubishi', 'Nissan', 'SEAT',
+       'Subaru', 'Suzuki', 'Toyota', 'Yamaha'] 
+
+    mileage: Union[int , float]
+    engine_power: Union[int, float ]
+
+    fuel: Literal['diesel', 'petrol', 'hybrid_petrol', 'electro']
+
+    paint_color: Literal['black', 'grey', 'white', 'red', 'silver', 'blue', 'orange',
+       'beige', 'brown', 'green']
+
+    car_type: Literal['convertible', 'coupe', 'estate', 'hatchback', 'sedan',
+       'subcompact', 'suv', 'van']
+
+    private_parking_available: Literal[True, False]
+
+    has_gps: Literal[True, False]
+
+    has_air_conditioning: Literal[True, False]
+
+    automatic_car: Literal[True, False]
+
+    has_getaround_connect: Literal[True, False]
+
+    has_speed_regulator : Literal[True, False]
+
+    winter_tires : Literal[True, False]
+
+    """
+  
+#we train the model & use his predictions to the predict endpoint of our API
+    data = pd.read_csv("get_around_pricing_project.csv")
+    data = data.drop(labels = ['Unnamed: 0'], axis = 1)
+    data = data[data['mileage'] >= 0]
+    data[['model_key','fuel','paint_color','car_type']] = data[['model_key','fuel','paint_color','car_type']].astype('string')
+
+#separate features with target
+    X = data.iloc[:, :-1]
+    y = data.iloc[:, -1:]
+
+#finding numerical and categorical value to encode or normalize them
+    numeric_features = []
+    categorical_features = []
+    for i,t in X.dtypes.iteritems():
+        if ('float' in str(t)) or ('int' in str(t)) :
+            numeric_features.append(i)
+        else :
+            categorical_features.append(i)
+
+#make the train /test split
+    X_train, X_test, Y_train, Y_test = train_test_split(X, y,test_size=0.2,
+                                                        random_state=0)
+    
+
+                
+    # Create pipeline for numeric features
+    numeric_transformer = Pipeline(steps=[
+        ('imputer', SimpleImputer(strategy='mean')), # missing values will be replaced by columns' mean
+        ('scaler', StandardScaler())
+    ])
+
+    # Create pipeline for categorical features
+    categorical_transformer = Pipeline(
+    steps=[
+    ('encoder',OneHotEncoder(drop='first',handle_unknown='ignore')) # first column will be dropped to avoid creating correlations between features
+    ])
+    # Use ColumnTransformer to make a preprocessor object that describes all the treatments to be done
+    preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, numeric_features),
+        ('cat', categorical_transformer, categorical_features)
+    ])
+
+    X_train = preprocessor.fit_transform(X_train)
+    X_test = preprocessor.transform(X_test)
+
+#we train the model
+    regressor = LinearRegression()
+    model = regressor.fit(X_train, Y_train)
+   
+ #we apply the same preprocessing on datas that would be submit to the API
+ #in a POST request to predict a rental price per day
+     
+    df = pd.DataFrame(dict(predictionFeatures), index=[0])
+    numeric_features = []
+    categorical_features = []
+    for i,t in df.dtypes.iteritems():
+            if ('float' in str(t)) or ('int' in str(t)) :
+                numeric_features.append(i)
+            else :
+                categorical_features.append(i)
+
+    df = preprocessor.transform(df)
+
+    # Predictions on df set
+    prediction = np.round(model.predict(df),2)
+    
+    # Format response
+    response = {"prediction rental price per day": prediction.tolist()}
+    return response
+
+
+#on utilise le serveur uvicorn pour faire tourner notre instance de FastApi
+#heroku n'accepte que les serveurs gunicorn
+
+if __name__=="__main__":
+    uvicorn.run(app, host="0.0.0.0", port=4000, debug=True, reload=True)
